7eea54e90395269c8321731233c87f6071e80151
Shiloli-r <ronaldshiloli@gmail.com>
2023-09-07 15:39:43 +0300

    token generation work in progress

--- ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/controller/AuthenticationController.java
+++ ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/controller/AuthenticationController.java
@@ -1,12 +1,13 @@
 package com.validatepolicy.ValidatePolicy.controller;
 
-import com.validatepolicy.ValidatePolicy.model.Authentication;
+import com.validatepolicy.ValidatePolicy.response.ErrorDesc;
+import com.validatepolicy.ValidatePolicy.request.RequestPayload;
+import com.validatepolicy.ValidatePolicy.response.ResponsePayload;
 import com.validatepolicy.ValidatePolicy.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="api/Solid/SubmitRequest")
@@ -15,23 +16,27 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @Autowired
-    public AuthenticationController(AuthenticationService authenticationService){
+    public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
-    @PostMapping
-    public String generateToken(@RequestBody Authentication authentication){
-       final String token = authenticationService.generateToken();
-       final String expiry = authenticationService.generateTimeStamp();
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<ResponsePayload> processRequest(@RequestBody RequestPayload requestPayload) {
+        // Validate the API user and password
 
-       return "{\n" +
-               "\"error_code\": \"00\",\n" +
-               "\"error_desc\": {\n" +
-               "\"token\": \"" + token + "\",\n" +
-               "\"expiry\": \"" + expiry +"\"\n" +
-               "}\n" +
-               "}";
+        // Create the response payload
+        final String token = authenticationService.generateToken();
+        final String expiry = authenticationService.generateTimeStamp();
 
+        ResponsePayload response = new ResponsePayload();
+        response.setError_code("00");
+
+        ErrorDesc errorDesc = new ErrorDesc();
+        errorDesc.setToken(token);
+        errorDesc.setExpiry(expiry);
+
+        response.setError_desc(errorDesc);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
 }
