e0622144802f170974c06f04be73823bb33acca3
Shiloli-r <ronaldshiloli@gmail.com>
2023-09-07 17:05:24 +0300

    user and password validation

--- ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/controller/AuthenticationController.java
+++ ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.validatepolicy.ValidatePolicy.controller;
 
+import com.validatepolicy.ValidatePolicy.model.Authentication;
 import com.validatepolicy.ValidatePolicy.response.ErrorDesc;
 import com.validatepolicy.ValidatePolicy.request.RequestPayload;
 import com.validatepolicy.ValidatePolicy.response.ResponsePayload;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping(path="api/Solid/SubmitRequest")
@@ -22,21 +25,32 @@ public class AuthenticationController {
 
     @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<ResponsePayload> processRequest(@RequestBody RequestPayload requestPayload) {
+        ResponsePayload response = new ResponsePayload();
+
         // Validate the API user and password
 
-        // Create the response payload
-        final String token = authenticationService.generateToken();
-        final String expiry = authenticationService.generateTimeStamp();
+        for (Authentication authentication: authenticationService.authList) {
+            if(Objects.equals(authentication.getApi_user(), requestPayload.getMessageValidation().getApiUser()) && Objects.equals(authentication.getApi_password(), requestPayload.getMessageValidation().getApiPassword())) {
+                // TODO: Check interface
 
-        ResponsePayload response = new ResponsePayload();
-        response.setError_code("00");
+                // Create the response payload
+                final String token = authenticationService.generateToken();
+                final String expiry = authenticationService.generateTimeStamp();
+                response.setError_code("00");
 
-        ErrorDesc errorDesc = new ErrorDesc();
-        errorDesc.setToken(token);
-        errorDesc.setExpiry(expiry);
+                ErrorDesc errorDesc = new ErrorDesc();
+                errorDesc.setToken(token);
+                errorDesc.setExpiry(expiry);
 
-        response.setError_desc(errorDesc);
+                response.setError_desc(errorDesc);
 
+            }
+            else{
+                // create the response payload
+                response.setError_code("01");
+                // TODO: Set Error Desc to "Invalid API consumer"
+            }
+        }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
