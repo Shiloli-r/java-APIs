02794ba48995c8eab946dee60d63036a08cf1786
Shiloli-r <ronaldshiloli@gmail.com>
2023-09-07 17:03:41 +0300

    dropped token and expiry properties

--- ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/model/Authentication.java
+++ ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/model/Authentication.java
@@ -3,8 +3,6 @@ package com.validatepolicy.ValidatePolicy.model;
 public class Authentication {
     private String api_user;
     private String api_password;
-    private String token;
-    private String expiry;
 
     public Authentication() {
     }
@@ -12,13 +10,6 @@ public class Authentication {
     public Authentication(String api_user, String api_password) {
         this.api_user = api_user;
         this.api_password = api_password;
-    }
-
-    public Authentication(String api_user, String api_password, String token, String expiry) {
-        this.api_user = api_user;
-        this.api_password = api_password;
-        this.token = token;
-        this.expiry = expiry;
     }
 
     public String getApi_user() {
@@ -36,20 +27,6 @@ public class Authentication {
     public void setApi_password(String api_password) {
         this.api_password = api_password;
     }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getExpiry() {
-        return expiry;
-    }
-
-    public void setExpiry(String expiry) {
-        this.expiry = expiry;
-    }
 }
+
+
