931b0abf0894a2414e6f5ef38c8de7b1aec3725a
Shiloli-r <ronaldshiloli@gmail.com>
2023-09-11 11:23:09 +0300

    Validate Policy

--- ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/controller/ValidatePolicyController.java
+++ ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/controller/ValidatePolicyController.java
@@ -1,10 +1,12 @@
 package com.validatepolicy.ValidatePolicy.controller;
 
 import com.validatepolicy.ValidatePolicy.model.Authentication;
+import com.validatepolicy.ValidatePolicy.model.Policy;
 import com.validatepolicy.ValidatePolicy.response.ErrorDesc;
 import com.validatepolicy.ValidatePolicy.request.RequestPayload;
-import com.validatepolicy.ValidatePolicy.response.AuthResponsePayload;
+import com.validatepolicy.ValidatePolicy.response.ResponsePayload;
 import com.validatepolicy.ValidatePolicy.service.AuthenticationService;
+import com.validatepolicy.ValidatePolicy.service.PolicyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,23 +19,23 @@ import java.util.Objects;
 public class ValidatePolicyController {
 
     private final AuthenticationService authenticationService;
+    private final PolicyService policyService;
 
     @Autowired
-    public ValidatePolicyController(AuthenticationService authenticationService) {
+    public ValidatePolicyController(AuthenticationService authenticationService, PolicyService policyService) {
         this.authenticationService = authenticationService;
+        this.policyService = policyService;
     }
 
     @PostMapping(produces = "application/json", consumes = "application/json")
-    public ResponseEntity<AuthResponsePayload> processRequest(@RequestBody RequestPayload requestPayload) {
-        AuthResponsePayload response = new AuthResponsePayload();
+    public ResponseEntity<ResponsePayload> processRequest(@RequestBody RequestPayload requestPayload) {
+        ResponsePayload response = new ResponsePayload();
 
         // check interface --> if TOKEN, then generate token elseif CUSTOMER_INFO, validate policy
         if (Objects.equals(requestPayload.getMessageRoute().getInterfaceName(), "TOKEN")) {
             // Validate the API user and password
             for (Authentication authentication : authenticationService.authList) {
                 if (Objects.equals(authentication.getApi_user(), requestPayload.getMessageValidation().getApiUser()) && Objects.equals(authentication.getApi_password(), requestPayload.getMessageValidation().getApiPassword())) {
-                    // TODO: Check interface
-
                     // Create the response payload
                     final String token = authenticationService.generateToken();
                     final String expiry = authenticationService.generateTimeStamp();
@@ -53,9 +55,38 @@ public class ValidatePolicyController {
                 }
             }
         } else if (Objects.equals(requestPayload.getMessageRoute().getInterfaceName(), "CUSTOMER_INFO")) {
-            // Validate the policy
-        }
+            for (Policy policy : policyService.policyList){
+                if(Objects.equals(policy.getPolicyNo(), requestPayload.getMessageBody().getPolicyNo()) && Objects.equals(policy.getBankCode(), requestPayload.getMessageBody().getBankCode())){
+                    // policy number and Bank Code exist
+                    response.setError_code("00");
 
+                    ErrorDesc errorDesc = new ErrorDesc();
+
+                    errorDesc.setMessage("VALID");
+                    errorDesc.setStatusCode("00");
+                    errorDesc.setAccountNo(policy.getAccNo());
+                    errorDesc.setCustomerName(policy.getCustomerName());
+                    errorDesc.setOutstandingAmount(null);
+                    errorDesc.setDueDate(null);
+
+                    response.setError_desc(errorDesc);
+                    break;
+                } else{
+                    response.setError_code("00");
+                    ErrorDesc errorDesc = new ErrorDesc();
+
+                    errorDesc.setMessage("INVALID");
+                    errorDesc.setReferenceNumber("1349");
+                    errorDesc.setCustomerName("");
+                    errorDesc.setOutstandingAmount(null);
+                    errorDesc.setDueDate(null);
+                    errorDesc.setStatusCode("01");
+
+                    response.setError_desc(errorDesc);
+                }
+
+            }
+        }
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
