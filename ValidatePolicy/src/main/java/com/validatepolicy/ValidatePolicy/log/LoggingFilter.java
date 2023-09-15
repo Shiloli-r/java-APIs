84834b98681e41813dd1360713e11ea694bea256
Shiloli-r <ronaldshiloli@gmail.com>
2023-09-15 11:14:05 +0300

    log payloads

---
+++ ValidatePolicy/src/main/java/com/validatepolicy/ValidatePolicy/log/LoggingFilter.java
@@ -0,0 +1,51 @@
+package com.validatepolicy.ValidatePolicy.log;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws jakarta.servlet.ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper((jakarta.servlet.http.HttpServletRequest) request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((jakarta.servlet.http.HttpServletResponse) response);
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter((ServletRequest) requestWrapper, (ServletResponse) responseWrapper);
+        long timeTaken = System.currentTimeMillis() - startTime;
+
+        String requestBody = getStringValue(requestWrapper.getContentAsByteArray(),
+                request.getCharacterEncoding());
+        String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
+                response.getCharacterEncoding());
+
+        LOGGER.info(
+                "METHOD={}; URI={}; REQUEST PAYLOAD={}; RESPONSE CODE={}; RESPONSE PAYLOAD={}; TIME TAKEN={}",
+                request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody,
+                timeTaken);
+        responseWrapper.copyBodyToResponse();
+    }
+
+
+    private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
+        try {
+            return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
